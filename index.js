e29ae76e64ecef3eb39db7d4ba0cce49a8d5a14c
Ben Geier <geier.benjamin@gmail.com>
2024-02-16 20:31:42 -0500

    finish lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names,event){
+    let messages= []
+    for (let i=0; i <names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    debugger;
+    return messages
+}
+
+writeCards(["Alexander", "Nicklas", "Tom"], "Stanley Cup celebration");
+
+function countDown(n){
+    let i= n
+    while (i>=0){
+        console.log(i--)
+    }
+}
